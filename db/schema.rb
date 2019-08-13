f34f801ede50f83b28c6352d1730a9542e0ef781
Joris Dewaele <joris.dewaele@tutanota.com>
2019-08-13 15:26:42 +0200

    Removed picture column from User model, replaced by photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_120619) do
+ActiveRecord::Schema.define(version: 2019_08_13_131839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2019_08_13_120619) do
     t.string "name"
     t.string "lastname"
     t.string "address"
-    t.string "picture"
     t.string "payment"
     t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
