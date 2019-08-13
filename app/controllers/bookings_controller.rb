569a93c11418a8c06649458a1b14d38b068ac7f2
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-13 18:02:08 +0200

    create method, set_user method and changed routes for the create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,17 +2,38 @@ class BookingsController < ApplicationController
   # sets the variable @booking for show
   before_action :set_booking, only: [:show]
   # sets the variable @dog for new
-  before_action :set_dog, only: [:new]
+  before_action :set_dog, only: [:new, :create]
+  # sets the variable @user for new and create
+  before_action :set_user, only: [:new, :create]
 
   def show
   end
 
   def new
+    # pass an empty booking instance to the view
     @booking = Booking.new
-    @user = current_user
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    # set the user and dog to the booking
+    @booking.user = @user
+    @booking.dog = @dog
+
+    # if the booking is saved successfully, redirect to the booking show page
+    if @booking.save!
+      redirect_to booking_path(@booking)
+    # otherwise render new form
+    else render :new
+    end
   end
 
   private
+
+  def set_user
+    # @user variable is equal to the user that is currently logged in
+    @user = current_user
+  end
 
   def set_booking
     # query the booking instance from the DB using the params
@@ -20,6 +41,11 @@ class BookingsController < ApplicationController
   end
 
   def set_dog
+    # query the dog instance from the DB using the params
     @dog = Dog.new(params[:dog_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:description, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
     member do
       get 'preview'
     end
-    resources :bookings, only: [:new]
+    resources :bookings, only: [:new, :create]
   end
   # can see details of one booking, create and delete.
-  resources :bookings, only: [:show, :create, :delete]
+  resources :bookings, only: [:show, :delete]
   # can create a review
   resources :reviews, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
