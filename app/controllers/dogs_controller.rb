6e17484b8b1c94275985ab40ffe0ea8710df4964
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-13 12:35:15 +0200

    create method, edit method, set_owner method, set_dog method, dog_params method

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,7 +1,8 @@
 class DogsController < ApplicationController
+  before_action :set_owner, only: [:show]
+  before_action :set_dog, only: [:show, :edit]
+
   def show
-    # give the dog instance to the view
-    @dog = Dog.find(params[:id])
     # give the bookings of the dog as an array to the views
     @bookings = @dog.bookings
     # give the owner of the dog to the views
@@ -12,14 +13,47 @@ class DogsController < ApplicationController
   end
 
   def index
+    # return array of all the dogs in the DB
     @dogs = Dog.all
   end
 
   def new
+    #pass a new dog empty dog instance to the view
     @dog = Dog.new
-    @user = User.find(params[:id])
   end
 
   def create
+    # create the dog with the params from the form
+    @dog = Dog.new(dog_params)
+    # find the owner of the dog from the params of the form
+    @user = User.find(params[:user_id])
+    # set the owner to the dog
+    @dog.user = @user
+
+    # redirects to the show page of the dog if the create is successfull otherwise render new form
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_dog
+    # give the dog instance to the view
+    @dog = Dog.find(params[:id])
+  end
+
+  def set_owner
+    # give the owner instance to the view
+    @owner = User.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :race, :size, :description, :available, :image)
   end
 end
