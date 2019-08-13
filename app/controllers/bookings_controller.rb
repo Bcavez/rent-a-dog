f25132fc17c8d8fb35bbfdde4ece04957818707d
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-13 17:48:25 +0200

    set_dog method, new method and changed routes for the new method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,15 @@
 class BookingsController < ApplicationController
   # sets the variable @booking for show
   before_action :set_booking, only: [:show]
+  # sets the variable @dog for new
+  before_action :set_dog, only: [:new]
 
   def show
+  end
+
+  def new
+    @booking = Booking.new
+    @user = current_user
   end
 
   private
@@ -10,5 +17,9 @@ class BookingsController < ApplicationController
   def set_booking
     # query the booking instance from the DB using the params
     @booking = Booking.new(params[:id])
+  end
+
+  def set_dog
+    @dog = Dog.new(params[:dog_id])
   end
 end
