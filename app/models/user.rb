0dd9694a6758d3ad2adc80285c1e1c0950bc90ac
Joris Dewaele <joris.dewaele@tutanota.com>
2019-08-13 12:29:48 +0200

    Device login sanitazed with user Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :name, presence: true
   validates :lastname, presence: true
   validates :email, uniqueness: true
   validates :address, presence: true
+
   has_many :dogs, dependent: :destroy
   has_many :bookings, dependent: :destroy
 end
