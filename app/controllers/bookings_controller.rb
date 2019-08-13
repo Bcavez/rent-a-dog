3998d969fb39927a6086661a8997db72598a25fe
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-13 17:43:00 +0200

    created set_booking method and show method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+  # sets the variable @booking for show
+  before_action :set_booking, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_booking
+    # query the booking instance from the DB using the params
+    @booking = Booking.new(params[:id])
+  end
 end
