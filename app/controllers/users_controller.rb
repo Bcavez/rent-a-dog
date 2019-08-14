5915e0db8a7ede8c81151686ddc446b9d8f3de68
Joris Dewaele <joris.dewaele@tutanota.com>
2019-08-15 01:30:03 +0200

    Pundit updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,22 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :dashboard]
   def show
-    @dogs = @user.dogs
-    @bookings = @user.bookings
-
-    authorize @user
+    @dogs = policy_scope(Dog)
+    @bookings = policy_scope(Booking)
   end
 
   def dashboard
     # a user can see all of his bookings
     @bookings = BookingPolicy::Scope.new(current_user, Booking).resolve
+    # @bookings = policy_scope(Booking)
     # send an array of all the dogs associated with the bookings to the views
     @dogs = @bookings.map(&:dog)
     # send an array of all the renter associated with the bookings to the views
     @renter = @bookings.map(&:user)
-
-    authorize @bookings
-    # authorize @user
   end
 
   def set_user
     @user = current_user
+    authorize @user
   end
 end
