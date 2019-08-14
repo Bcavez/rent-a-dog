5d886dc43c61131f5f6cd0b2a871abe898aab40a
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-14 16:05:23 +0200

    dahsboard fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :dashboard]
   def show
-    @user = current_user
-
     @dogs = @user.dogs
     @bookings = @user.bookings
 
     authorize @user
+  end
 
+  def dashboard
+    # a user can see all of his bookings
+    @bookings = Booking.where(user_id: @user[:id])
+    # send an array of all the dogs associated with the bookings to the views
+    @dogs = @bookings.map(&:dog)
+    # send an array of all the renter associated with the bookings to the views
+    @renter = @bookings.map(&:user)
+
+    # authorize @user
+  end
+
+  def set_user
+    @user = current_user
   end
 end
