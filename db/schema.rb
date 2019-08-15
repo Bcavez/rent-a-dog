b89a71e0f9f451b5b8d818941c6d2c8cf4cc5d9d
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-15 14:23:15 +0200

    migration setting default value of rating to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_105255) do
+ActiveRecord::Schema.define(version: 2019_08_15_121939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_105255) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "description"
-    t.integer "rating"
+    t.integer "rating", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "dog_id"
