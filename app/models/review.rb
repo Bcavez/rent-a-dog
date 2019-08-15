4b44579813ef6fa514a3d393cb22d366477a2401
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-15 12:55:18 +0200

    added models associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   validates :description, presence: true
   validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5], message: "rating must be between 0 and 5" }
   belongs_to :dog
+  belongs_to :user
 end
