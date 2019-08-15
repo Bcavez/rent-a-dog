5024e892feca45383a2b2e5902d6902123db0e08
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-15 12:35:15 +0200

    created average_rating method

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,4 @@
 class Dog < ApplicationRecord
-
-
-
   validates :name, presence: true
   validates :race, presence: true
   validates :size, presence: true, inclusion: { in: %w(small medium large),
@@ -12,4 +9,15 @@ class Dog < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
 
+  def average_rating(dog)
+    # replace the dog with an array of his reviews
+    array = dog.reviews
+    # replace each review object by the rating integer
+    array.map(&:rating)
+    # compute the average
+    length = array.length
+    average = array.sum / length
+    # return the average rating float
+    return average
+  end
 end
